6ece34a9b83c807458fb9aa8e841f852159d28e9
unknown <arlauskaslukas@gmail.com>
2020-03-04 07:56:14 -0800

    added blank eshop page

---
+++ frontend/src/pages/shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const title = 'Shop';
+
+function Shop() {
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
+      <h1>Shop</h1>
+    </>
+  );
+}
+
+export default Shop;
