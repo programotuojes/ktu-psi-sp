b6f1c51506763611822117959c3dcd87732bf309
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2020-03-09 16:50:00 +0200

    PSI-7 Change navigation bar design

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,16 +2,24 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import { Toolbar } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import logo from '../logo.svg';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles({
   grow: {
     flexGrow: 1,
   },
-}));
+  icon: {
+    marginLeft: '6px',
+    padding: '0px',
+  },
+  nav: {
+    'font-size': '1.2rem',
+    paddingLeft: '24px',
+    paddingRight: '24px',
+  },
+});
 
 function NavBar() {
   const classes = useStyles();
@@ -20,20 +28,25 @@ function NavBar() {
     <>
       <AppBar>
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="categories">
-            <MenuIcon />
+          <IconButton className={classes.icon} href="/" aria-label="home">
+            <img src={logo} alt="logo" width={'50px'} height={'50px'} />
           </IconButton>
-
-          <IconButton>
-            <img src={logo} alt={'logo'} width={50} height={50} />
-          </IconButton>
-
-          <Button>Blog</Button>
-          <Button>E-shop</Button>
 
           <div className={classes.grow} />
 
-          <Button>About</Button>
+          <Button className={classes.nav} href="/blog">
+            Blog
+          </Button>
+
+          <div className={classes.grow} />
+
+          <Button className={classes.nav} href="/shop">
+            Shop
+          </Button>
+
+          <div className={classes.grow} />
+
+          <Button href="/about">About</Button>
         </Toolbar>
       </AppBar>
       <Toolbar />
