8278807ed4965251500e38f9de13f2bb9b48f1a5
Vaiva <vaidilaite.vaiva@gmail.com>
2020-03-18 16:01:46 +0200

    PSI-38and48

--- frontend/src/pages/homepage/Homepage.js
+++ frontend/src/pages/homepage/Homepage.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import {Helmet} from 'react-helmet';
 import ImageButton from './ImageButton';
 import blogLogo from './blogLogo.png';
 import shopLogo from './shopLogo.jpg';
 import Grid from '@material-ui/core/Grid';
+import {makeStyles} from '@material-ui/core/styles';
 
 const title = 'Homepage';
 
-function Homepage() {
+const useStyles = makeStyles({
+  blog_header: {
+    position: 'absolute',
+    top: '70%',
+    left: '23%',
+    color: 'orange',
+    'font-family': 'Georgia, serif',
+    'font-style': 'italic',
+    'letter-spacing': '1px',
+    'font-size': '53px',
+    'text-shadow': '-6px 0 black, 0 3px black, 1px 0 black, 0 -3px black',
+  },
 
+  shop_header: {
+    position: 'absolute',
+    top: '70%',
+    left: '66%',
+    color: 'grey',
+    'font-family': 'Georgia, serif',
+    'font-style': 'italic',
+    'letter-spacing': '1px',
+    'font-size': '53px',
+    'text-shadow': '-5px 0 black, 0 2px black, 1px 0 black, 0 -3px black',
+  },
+});
+
+function Homepage() {
+  const classes = useStyles();
   return (
     <>
       <Helmet>
@@ -16,10 +43,11 @@ function Homepage() {
       </Helmet>
 
       <h1>Homepage</h1>
-        <Grid container spacing={2}>
-                <ImageButton image={blogLogo} url={"/blog"} />
-                <ImageButton image={shopLogo} url={"/shop"} />
-        </Grid>
+      <Grid container spacing={2}>
+        <ImageButton image={blogLogo} url={'/blog'} text={'Blogas'} style={classes.blog_header} />
+        <ImageButton image={shopLogo} url={'/shop'} text={'E-parduotuvė'} style={classes.shop_header}
+        />
+      </Grid>
     </>
   );
 }
--- frontend/src/pages/homepage/ImageButton.js
+++ frontend/src/pages/homepage/ImageButton.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 function ImageButton(props) {
-
-    return (
-    <a href={props.url} >
-        <img src={props.image} alt={'Page logo'}/>
+  return (
+    <a href={props.url}>
+      <img src={props.image} alt={'Page logo'} />
+      <h2 className={props.style}>{props.text}</h2>
     </a>
   );
 }
