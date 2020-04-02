0bcc7e1239f439ae9da99d0435cf0177fbd538be
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2020-04-02 22:35:08 +0300

    PSI-11 Show menu button in the shop page

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,10 +3,17 @@ import AppBar from '@material-ui/core/AppBar';
 import { Toolbar } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
 import logo from '../logo.svg';
 import { makeStyles } from '@material-ui/core/styles';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleSidebar } from '../store/actions/sidebar';
+import { isSidebarOpen } from '../store/selectors/sidebar';
 
 const useStyles = makeStyles({
+  appBar: {
+    zIndex: 3000,
+  },
   grow: {
     flexGrow: 1,
   },
@@ -23,11 +30,30 @@ const useStyles = makeStyles({
 
 function NavBar() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const open = useSelector(isSidebarOpen);
+
+  const sidebarButton = () => {
+    if (window.location.pathname === '/shop') {
+      return (
+        <IconButton
+          aria-label="open sidebar"
+          onClick={() => dispatch(toggleSidebar(!open))}
+          edge="start"
+        >
+          <MenuIcon />
+        </IconButton>
+      );
+    }
+  };
 
   return (
     <>
-      <AppBar>
+      <AppBar className={classes.appBar}>
         <Toolbar>
+          {sidebarButton()}
+
           <IconButton className={classes.icon} href="/" aria-label="home">
             <img src={logo} alt="logo" width={'50px'} height={'50px'} />
           </IconButton>
