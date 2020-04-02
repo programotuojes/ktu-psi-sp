6b8ddefe7e91ccba4e3774459b109d73d781e744
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2020-04-02 23:23:15 +0300

    PSI-11 Display actual categories in sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -2,16 +2,14 @@ import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import Divider from '@material-ui/core/Divider';
 import { toggleSidebar } from '../store/actions/sidebar';
 import { Toolbar } from '@material-ui/core';
 import { isSidebarOpen } from '../store/selectors/sidebar';
+import { CATEGORIES } from '../constants';
 
 const useStyles = makeStyles({
   list: {
@@ -23,26 +21,26 @@ function Sidebar() {
   const classes = useStyles();
   const dispatch = useDispatch();
 
+  const categories = Object.values(CATEGORIES);
   const open = useSelector(isSidebarOpen);
-
-  function list() {
-    return (
-      <List className={classes.list}>
-        {}
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    );
-  }
 
   return (
     <Drawer open={open} onClose={() => dispatch(toggleSidebar(false))}>
       <Toolbar />
-      {list()}
+
+      <List className={classes.list}>
+        <ListItem button key={categories[0]}>
+          <ListItemText primary={categories[0]} />
+        </ListItem>
+
+        <Divider />
+
+        {categories.slice(1, -1).map((text) => (
+          <ListItem button key={text} onClick={}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
     </Drawer>
   );
 }
