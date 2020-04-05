c4ab0aa2069c2577773c15d8b54b8564118c4e58
Joksalt <Jketura@gmail.com>
2020-04-05 23:10:48 +0300

    PSI-44 Create item grid

--- frontend/src/pages/shop/Shop.js
+++ frontend/src/pages/shop/Shop.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { makeStyles } from '@material-ui/core/styles';
 import ItemTemplate from './ItemTemplate';
-import img from './jacket1.jpg';
-import img2 from './jumper.jpg';
-import img3 from './shorts.jpg';
+import shopData from './shopData'
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 const title = 'Shop';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 function Shop() {
+  const classes = useStyles();
+
   return (
     <>
       <Helmet>
         <title>{title}</title>
       </Helmet>
-
-      <ItemTemplate image={img} title={'jacket'} price={2000} />
-      <ItemTemplate image={img2} title={'jumper'} price={5005} />
-      <ItemTemplate image={img3} title={'shorts'} price={15014} />
+      {ListItemsInGrid(shopData,classes)}
     </>
   );
 }
 
 export default Shop;
+
+/*
+Method, which lists items in a grid.
+Takes in two arguments:
+props - array of data.
+style - style class.
+ */
+function ListItemsInGrid(props, style) {
+  return(
+    <div className={style.root}>
+      <GridList className={style.gridList} cellHeight={500} cols={3}>
+        {shopData.map((props) => (
+          <GridListTile>
+            <ItemTemplate image={props.img} title={props.title} price={props.price} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  )
+}
