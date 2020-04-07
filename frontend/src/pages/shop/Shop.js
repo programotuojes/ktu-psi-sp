1c865aa5e87de256a126542eedab84f82ddedcb0
Joksalt <Jketura@gmail.com>
2020-04-07 15:42:09 +0300

    PSI-44 Create item grid

--- frontend/src/pages/shop/Shop.js
+++ frontend/src/pages/shop/Shop.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { makeStyles } from '@material-ui/core/styles';
-import ItemTemplate from './ItemTemplate';
-import shopData from './shopData'
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
+import shopData from './shopData';
+import ItemGrid from './ItemGrid';
 
 const title = 'Shop';
 
@@ -27,29 +25,9 @@ function Shop() {
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      {ListItemsInGrid(shopData,classes)}
+      {ItemGrid(shopData, classes)}
     </>
   );
 }
 
 export default Shop;
-
-/*
-Method, which lists items in a grid.
-Takes in two arguments:
-props - array of data.
-style - style class.
- */
-function ListItemsInGrid(props, style) {
-  return(
-    <div className={style.root}>
-      <GridList className={style.gridList} cellHeight={500} cols={3}>
-        {shopData.map((props) => (
-          <GridListTile>
-            <ItemTemplate image={props.img} title={props.title} price={props.price} />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
-  )
-}
