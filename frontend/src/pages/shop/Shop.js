a8bf80aaf0ad368b6c4eea16b0c42b515276e02f
Joksalt <Jketura@gmail.com>
2020-04-07 16:21:21 +0300

    Create item grid

--- frontend/src/pages/shop/Shop.js
+++ frontend/src/pages/shop/Shop.js
@@ -12,11 +12,6 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
 }));
 
 function Shop() {
