b48a27027ae242fccf3faa2277a84eb2ff2b5984
unknown <arlauskaslukas@gmail.com>
2020-04-07 15:54:29 +0300

    PSI-18

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
-
 import theme from './theme';
 import Homepage from './pages/homepage/Homepage';
 import Shop from './pages/shop/Shop';
@@ -9,23 +8,26 @@ import NavBar from './components/NavBar';
 import About from './pages/about/About';
 import Blog from './pages/blog/Blog';
 import Payment from './pages/payment/Payment';
-import ProductPageTemplate from "./pages/shop/ProductPageTemplate";
+import ProductPage from "./pages/shop/ProductPage";
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <NavBar />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={'/'} component={Homepage} />
-          <Route exact path={'/shop'} component={Shop} />
-          <Route exact path={'/about'} component={About} />
-          <Route exact path={'/blog'} component={Blog} />
-          <Route exact path={'/payment'} component={Payment} />
-          <Route exact path={'/shop/ProductPageTemplate'} component={ProductPageTemplate} />
-        </Switch>
-      </BrowserRouter>
-    </ThemeProvider>
+      <HelmetProvider>
+        <ThemeProvider theme={theme}>
+          <NavBar />
+          <BrowserRouter>
+            <Switch>
+              <Route exact path={'/'} component={Homepage} />
+              <Route exact path={'/shop'} component={Shop} />
+              <Route exact path={'/about'} component={About} />
+              <Route exact path={'/blog'} component={Blog} />
+              <Route exact path={'/payment'} component={Payment} />
+              <Route exact path={'/shop/ProductPage'} component={ProductPage} />
+            </Switch>
+          </BrowserRouter>
+        </ThemeProvider>
+      </HelmetProvider>
   );
 }
 
--- frontend/src/pages/shop/ProductPage.js
+++ frontend/src/pages/shop/ProductPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import {Helmet} from 'react-helmet-async';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import img from './jacket1.jpg';
 
@@ -9,20 +9,21 @@ const useStyles = makeStyles({
     img: {
         height: 600,
         width: 500,
-        margin: 5,
+        margin: 30,
+        float:'left',
     },
     desc: {
         textAlign: 'center',
         padding: 20,
         paddingTop: 50,
     },
-    float: {
-        margin: 30,
-        float:'left',
-    },
+
     currency: {
         fontWeight: 'bold',
     },
+    text: {
+        marginTop: '32px',
+    }
 });
 
 const title = 'Some random product';
@@ -37,14 +38,12 @@ function ProductPage()
             </Helmet>
             <Grid container-spacing={3}>
                 <Grid item xs={12}>
-                    <Typography variant={'h4'} align={'center'}>
+                    <Typography variant={'h4'} align={'center'} className={classes.text}>
                         "Lorem ipsum"
                     </Typography>
                 </Grid>
                 <div align='center'>
-                    <Grid item xs={6} className={classes.float}>
-                        <img src={img} alt="Lorem ipsum" className={classes.img}/>
-                    </Grid>
+                    <img src={img} alt="Lorem ipsum" className={classes.img}/>
                     <Grid item xs={6}>
                         <Typography variant={'body1'} className={classes.desc}>
                             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam purus leo, eget ultrices augue mollis non. Donec in nisi vel libero condimentum condimentum sed."
