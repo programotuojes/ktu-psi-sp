a76fcc14492d5fbb0dc7ec6342ca99caa7690de6
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2020-04-08 11:06:40 +0300

    PSI-12 Add missing shopData.js file

---
+++ frontend/src/pages/shop/shopData.js
@@ -0,0 +1,16 @@
+import img from './jacket1.jpg';
+import img2 from './jumper.jpg';
+import img3 from './shorts.jpg';
+
+const shopData = [
+  { img: img, title: 'jacket', price: 2000 },
+  { img: img, title: 'jacket', price: 2000 },
+  { img: img2, title: 'jumper', price: 5005 },
+  { img: img2, title: 'jumper', price: 5005 },
+  { img: img, title: 'jacket', price: 2000 },
+  { img: img2, title: 'jumper', price: 5005 },
+  { img: img, title: 'jacket', price: 2000 },
+  { img: img3, title: 'shorts', price: 5005 },
+];
+
+export default shopData;
