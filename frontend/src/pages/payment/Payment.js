1dee61a3c8ddaf2353b67788e4599829f07672e0
smagumiske <57533242+smagumiske@users.noreply.github.com>
2020-04-27 23:25:18 +0300

    PSI-24. Add radio groups for delivery details and payment

--- frontend/src/pages/payment/Payment.js
+++ frontend/src/pages/payment/Payment.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles({
     margin: '16px',
     fontStyle: 'bold',
     textAlign: 'left',
-  }
+  },
 });
 const title = 'Payment';
 
@@ -42,14 +42,26 @@ function Payment() {
       <Grid container className={classes.root} cols={3}>
         <Grid item xs>
           <div className={classes.title}>Pirkėjo informacija</div>
-          <UserInformation/>
+          <UserInformation />
         </Grid>
         <Grid item xs>
           <div className={classes.title}>Pristatymo būdas</div>
           <RadioGroup name="delivery" value={valueDelivery} onChange={handleChangeDelivery}>
-            <FormControlLabel value="shop" control={<Radio />} label="Nemokamas atsiėmimas parduotuvėje. Rotušės a.15 Kaunas" />
-            <FormControlLabel value="courier" control={<Radio />} label="Pristatymas į namus per kurjerio tarnybą. Kaina 2.5€" />
-            <FormControlLabel value="postOffice" control={<Radio />} label="Atsiėmimas Lietuvos pašto skyriuje. Kaina 2.2€" />
+            <FormControlLabel
+              value="shop"
+              control={<Radio />}
+              label="Nemokamas atsiėmimas parduotuvėje. Rotušės a.15 Kaunas"
+            />
+            <FormControlLabel
+              value="courier"
+              control={<Radio />}
+              label="Pristatymas į namus per kurjerio tarnybą. Kaina 2.5€"
+            />
+            <FormControlLabel
+              value="postOffice"
+              control={<Radio />}
+              label="Atsiėmimas Lietuvos pašto skyriuje. Kaina 2.2€"
+            />
           </RadioGroup>
         </Grid>
         <Grid item xs>
@@ -60,7 +72,6 @@ function Payment() {
           </RadioGroup>
         </Grid>
       </Grid>
-
     </>
   );
 }
--- frontend/src/pages/payment/UserInformation.js
+++ frontend/src/pages/payment/UserInformation.js
@@ -1,77 +1,79 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import { makeStyles, responsiveFontSizes } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: 200,
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: 200,
+      },
     },
-  },
-}));
+  };
+});
+
 export default function UserInformation() {
   const classes = useStyles();
   return (
     <>
-        <form className={classes.root} autoComplete="off">
+      <form className={classes.root} autoComplete="off">
+        <div>
           <div>
-            <div>
-              <TextField
-                className={classes.textField}
-                required
-                id="Vardas"
-                label="Vardas"
-                defaultValue=""
-              />
-            </div>
-            <div>
-              <TextField
-                className={classes.textField}
-                required
-                id="Pavarde"
-                label="Pavardė"
-                defaultValue=""
-              />
-            </div>
-            <div>
-              <TextField
-                className={classes.textField}
-                id="El.pastas"
-                label="El.paštas"
-                defaultValue=""
-              />
-            </div>
-            <div>
-              <TextField
-                className={classes.textField}
-                required
-                id="Adresas"
-                label="Adresas"
-                defaultValue=""
-              />
-            </div>
-            <div>
-              <TextField
-                className={classes.textField}
-                required
-                id="Miestas"
-                label="Miestas"
-                defaultValue=""
-              />
-            </div>
-            <div>
-              <TextField
-                className={classes.textField}
-                required
-                id="Telefonas"
-                label="Telefonas"
-                defaultValue=""
-              />
-            </div>
+            <TextField
+              className={classes.textField}
+              required
+              id="Vardas"
+              label="Vardas"
+              defaultValue=""
+            />
           </div>
-        </form>
-      </>
-      );
+          <div>
+            <TextField
+              className={classes.textField}
+              required
+              id="Pavarde"
+              label="Pavardė"
+              defaultValue=""
+            />
+          </div>
+          <div>
+            <TextField
+              className={classes.textField}
+              id="El.pastas"
+              label="El.paštas"
+              defaultValue=""
+            />
+          </div>
+          <div>
+            <TextField
+              className={classes.textField}
+              required
+              id="Adresas"
+              label="Adresas"
+              defaultValue=""
+            />
+          </div>
+          <div>
+            <TextField
+              className={classes.textField}
+              required
+              id="Miestas"
+              label="Miestas"
+              defaultValue=""
+            />
+          </div>
+          <div>
+            <TextField
+              className={classes.textField}
+              required
+              id="Telefonas"
+              label="Telefonas"
+              defaultValue=""
+            />
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
