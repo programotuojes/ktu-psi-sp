0c985607fd8f62e4f2e17f3cb0f64697265e7237
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2020-05-12 20:04:23 +0300

    PSI-55 Add product size and quantity validation

--- frontend/src/utils/util.js
+++ frontend/src/utils/util.js
@@ -1,3 +1,31 @@
 export function formatPrice(price) {
   return new Intl.NumberFormat('lt-LT', { style: 'currency', currency: 'EUR' }).format(price / 100);
 }
+
+export function productSizeError(selectedSize) {
+  if (!selectedSize) {
+    return 'Pasirinkite dydį';
+  }
+
+  return false;
+}
+
+export function productQuantityError(selectedQuantity, quantityForSize) {
+  if (!selectedQuantity) {
+    return 'Įveskite kiekį';
+  }
+
+  if (selectedQuantity < 1) {
+    return 'Kiekis turi būti daugiau nei 0';
+  }
+
+  if (selectedQuantity % 1 !== 0) {
+    return 'Kiekis turi būti sveikas skaičius';
+  }
+
+  if (selectedQuantity > quantityForSize) {
+    return `Šio dydžio sandėlyje turime ${quantityForSize} vnt.`;
+  }
+
+  return false;
+}
