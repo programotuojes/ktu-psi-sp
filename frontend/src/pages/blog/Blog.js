79e0d6763b8e3f69ba0b17fcd7bfd80ce4d3b38a
Vaiva <vaidilaite.vaiva@gmail.com>
2020-05-19 15:24:54 +0300

    psi-69-blog

--- frontend/src/pages/blog/Blog.js
+++ frontend/src/pages/blog/Blog.js
@@ -1,7 +1,118 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Helmet } from 'react-helmet-async';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import blog1 from './blog1.jpg';
+import blog2 from './blog2.jpg';
+import blog3 from './blog3.jpg';
+import blog4 from './blog4.jpg';
+import blog5 from './blog5.jpg';
+import blog6 from './blog6.jpeg';
+import Card from '@material-ui/core/Card';
+import { post } from './posts.js';
 
 const title = 'Blog';
+
+//image desineje card'o puseje
+class BlogPost extends Component {
+  render() {
+    return (
+      <Card elevation={5} style={{ marginTop: '20px' }}>
+        <Grid
+          container
+          spacing={0}
+          styles={{
+            marginTop: '10px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Grid item sm={6} style={{ textAlign: 'center' }}>
+            <Typography style={{ textAlign: 'right', font: 'italic small-caps bold 15px cursive' }}>
+              {this.props.date}
+            </Typography>
+            <Typography
+              style={{
+                font: 'italic small-caps bold 15px cursive',
+                'text-decoration': 'underline',
+                'margin-bottom': '7px',
+              }}
+            >
+              {this.props.title}
+            </Typography>
+            <Typography
+              style={{ margin: '0px 10px 5px', font: 'italic small-caps bold 12px cursive' }}
+            >
+              {this.props.text}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            sm={6}
+            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+          >
+            <img src={this.props.img} style={{ width: '95%', height: '95%' }}></img>
+          </Grid>
+        </Grid>
+      </Card>
+    );
+  }
+}
+
+//image kaireje card'o puseje
+class BlogPost1 extends Component {
+  render() {
+    return (
+      <Card elevation={5} style={{ marginTop: '20px' }}>
+        <Grid
+          container
+          spacing={0}
+          styles={{
+            marginTop: '10px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Grid
+            item
+            sm={6}
+            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+          >
+            <img src={this.props.img} style={{ width: '95%', height: '95%' }}></img>
+          </Grid>
+          <Grid item sm={6} style={{ textAlign: 'center' }}>
+            <Typography
+              style={{
+                textAlign: 'right',
+                font: 'italic small-caps bold 15px cursive',
+                'margin-right': '10px',
+              }}
+            >
+              {this.props.date}
+            </Typography>
+            <Typography
+              style={{
+                font: 'italic small-caps bold 15px cursive',
+                'text-decoration': 'underline',
+                'margin-bottom': '7px',
+              }}
+            >
+              {this.props.title}
+            </Typography>
+            <Typography
+              style={{ margin: '0px 10px 5px', font: 'italic small-caps bold 12px cursive' }}
+            >
+              {this.props.text}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Card>
+    );
+  }
+}
 
 function Blog() {
   return (
@@ -10,7 +121,65 @@ function Blog() {
         <title>{title}</title>
       </Helmet>
 
-      <h1>Blog</h1>
+      <Container maxWidth={'xl'}>
+        <Grid
+          container
+          spacing={2}
+          style={{
+            flex: 1,
+            margin: '0px 0px 10px',
+            justifyContent: 'center',
+            alignItems: 'center',
+            'row-gap': '10%',
+          }}
+        >
+          <Grid item xs={12} sm={6}>
+            {' '}
+            {/*ant mazu ekranu visa eilute, ant dideliu puse eilutes*/}
+            <BlogPost
+              date={'2020.01.30'}
+              title={'Rožinė svajonė'}
+              img={blog1}
+              text={post.post1}
+              like={79}
+              view={220}
+              comments={32}
+            ></BlogPost>
+            <BlogPost1
+              date={'2020.03.05'}
+              title={'Bučinys'}
+              img={blog5}
+              text={post.post5}
+            ></BlogPost1>
+            <BlogPost
+              date={'2020.02.25'}
+              title={'Didysis išminčius'}
+              img={blog3}
+              text={post.post3}
+            ></BlogPost>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <BlogPost1
+              date={'2020.04.30'}
+              title={'Sapnas'}
+              img={blog4}
+              text={post.post4}
+            ></BlogPost1>
+            <BlogPost
+              date={'2020.02.03'}
+              title={'Kosmoso atletas'}
+              img={blog2}
+              text={post.post2}
+            ></BlogPost>
+            <BlogPost
+              date={'2020.02.22'}
+              title={'Sugrįžau namo'}
+              img={blog6}
+              text={post.post6}
+            ></BlogPost>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
---
+++ frontend/src/pages/blog/posts.js
@@ -0,0 +1,30 @@
+const post = {
+  post1:
+    'Rožinis dramblys, vienas žinomiausių Kauno gatvės meno kūrinių, prie kurio ne vienas jau turi ir savo nuotrauką!' +
+    ' Šis piešinys atsirado tada, kai menininkas Vytenis Jakas buvo įkvėptas ant šios sienos esančio užrašo “DEIMA+ARUNAS” ir vizualizavo svajones, ' +
+    'tokias dideles ir rožinės. O mums svajonės yra kažkas nuostabaus, jaukaus ir šilto. Todėl šiam drambliui įtaisėme naujus namus ' +
+    'arčiau kūno ir širdies! Leiskime svajonėms mums šildyti!',
+  post2:
+    'Koks geras tas kosmonautas! - šiandien išgirdome iš kliento. Gal ir Jūs turite draugą ar draugę, ' +
+    'kurie šiuo metu turi ypatingų siekių ir jiems praverstų toks pagalbininkas? Bent jau sušalti tikrai neleis, ' +
+    'o tikintiems dar ir pridės sėkmės! Juk tikėjimas - stipriausias įrankis judant savo tikslų link!',
+  post3:
+    'Ar žinote kaip gimė “The Wall” kolekcija? Pirmiausia tai iš meilės Kauno miestui, madai ir menui. ' +
+    'Taip nutiko, kad įkvėpimu tapo gatvės menas, kurio atvaizdai nukeliavo tiesiai ant jaukių džemperių, kelnių, marškinėlių ir kepurių! ' +
+    'The Wall dirba tiems, kuriems norisi, kad drabužis “kalbėtų” savo istoriją! Šiandien rodome senelį išminčių, kuris įprasmina išminties ' +
+    'ir meno sintezę, traukia akį atlikimo subtilumu ir žavi savo spalvomis bei paslaptimi... Iš kur ateina išmintis?',
+  post4:
+    'Menininko neofreską „Sapnas“, atliktą kartu su kolega klaipėdiečiu Edvardu Šepučiu ant VDA Klaipėdos fakulteto ' +
+    'sienos Daržų g., klaipėdiečiai jau spėjo pamėgti. Tai dvejų dalių neofreska  puošia dailės fakulteto namo fasadus. Kaip sakoma, ' +
+    'kur atsirasti meno kūriniui, kaip tik ne ant meno pastatų sienų?!',
+  post5:
+    'Ši freska buvo nutapyta 2017-06-02. Autoriai- dailininkai: Tadas Vincaitis – Plūgas iš Kauno ir Edvardas Šeputis – Shepa. ' +
+    'Šios freskos idėja kilo savivaldybei, kadangi gavo iš Valstybės turto fondo šį apleistą pastatą ant kurio buvo nutapyta freska. ' +
+    'Idėja buvo „miesto vartai“ ar „durys“, kuri pritrauktų kuo daugiau gyventojų bei turistų iš kitų rajonų. Gedimino g. 115C, Kaišiadorys.',
+  post6:
+    '2017 metų liepą gatvės meno festivalis NYKOKA surengė konkursą, pakviesdamas menininkus sukurti eskizus namo fasadui adresu Pilies gatvė Nr. 4. ' +
+    'Buvo atsiųsta išties įspūdingas skaičius eskizų, kuriuos kūrė dešimtys Kauno dailininkų, pateikdami daugybę vertų įgyvendinti meninių idėjų. Būsimas freskas ' +
+    'vertino solidi komisija. Laimėjo Tado Plūgo Vincaičio kūrinys "Sugrįžau namo". ',
+};
+
+export { post };
